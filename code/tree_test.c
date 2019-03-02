18015a986286faa160ed26bb1002a0d3c2562b9a
NickShermeister <ncsherman159@gmail.com>
2019-03-01 19:03:44 -0500

    Can now make red/black nodes with the opposite color of text on them, but need a completed node structure to work with.

--- code/tree_test.c
+++ code/tree_test.c
@@ -17,10 +17,12 @@ Color *make_color(double r, double b, double g){
   return c;
 }
 
-Color *red = make_color(0.69, 0.19, 0);
+
 int WINDOW_SIZE = 1500;
 int circle_height = 35;
 int count = 0;
+
+
 
 static void do_drawing(cairo_t *, GtkWidget *);
 
@@ -28,48 +30,58 @@ struct {
   gushort count;
 } glob;
 
-static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr,
-    gpointer user_data)
-{
+static gboolean on_draw_event(GtkWidget *widget, cairo_t *cr, gpointer user_data) {
   do_drawing(cr, widget);
 
   return FALSE;
 }
 
-static void do_drawing(cairo_t *cr, GtkWidget *widget)
-{
-  static gdouble const trs[8][8] = {
-      { 0.0, 0.15, 0.30, 0.5, 0.65, 0.80, 0.9, 1.0 },
-      { 1.0, 0.0,  0.15, 0.30, 0.5, 0.65, 0.8, 0.9 },
-      { 0.9, 1.0,  0.0,  0.15, 0.3, 0.5, 0.65, 0.8 },
-      { 0.8, 0.9,  1.0,  0.0,  0.15, 0.3, 0.5, 0.65},
-      { 0.65, 0.8, 0.9,  1.0,  0.0,  0.15, 0.3, 0.5 },
-      { 0.5, 0.65, 0.8, 0.9, 1.0,  0.0,  0.15, 0.3 },
-      { 0.3, 0.5, 0.65, 0.8, 0.9, 1.0,  0.0,  0.15 },
-      { 0.15, 0.3, 0.5, 0.65, 0.8, 0.9, 1.0,  0.0, }
-  };
-
+static void drawing_recursive(cairo_t *cr, GtkWidget *widget){
   GtkWidget *win = gtk_widget_get_toplevel(widget);
+  cairo_translate(cr, -2*circle_height, -2*circle_height);
+
+}
+
+/*
+  NOTES:
+  Cairo always saves last drawing point; need to be at the desired root location going into each drawing drawing_recursive
+  IT's a standard divide/conquer strategy?
+*/
+
+static void do_drawing(cairo_t *cr, GtkWidget *widget) {
+  GtkWidget *win = gtk_widget_get_toplevel(widget);
+  Color *red = make_color(0.69, 0.19, 0.0);
+  Color *black = make_color(0.0, 0.0, 0.0);
 
   gint width, height;
   gtk_window_get_size(GTK_WINDOW(win), &width, &height);
 
   cairo_translate(cr, width/2, circle_height + 5);
 
+  cairo_select_font_face(cr, "Purisa",
+      CAIRO_FONT_SLANT_NORMAL,
+      CAIRO_FONT_WEIGHT_BOLD);
+
+  cairo_set_font_size(cr, 13);
+
   gint i = 0;
   for (i = 0; i < glob.count; i++) {
-      cairo_translate(cr, (int) circle_height * 1.5, (int) circle_height * 1.5);
       cairo_set_line_width(cr, 3);
 
-      cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
-      cairo_set_source_rgba(cr, red->r, red->g, red->b);
-
+      // Circle
+      cairo_set_source_rgb(cr, red->r, red->g, red->b);
+      cairo_translate(cr, (int) circle_height * 1.5, (int) circle_height * 1.5);
       cairo_arc(cr, 0, 0, circle_height, 0, 2 * M_PI);
       cairo_stroke_preserve(cr);
 
-      cairo_set_source_rgb(cr, 0.69, 0.19, 0);
       cairo_fill(cr);
 
+      //Text
+      cairo_set_source_rgb(cr, black->r, black->g, black->b);
+      cairo_show_text(cr, "40");
+      cairo_fill(cr);
+
+      // drawing_recursive(cr, widget);
       // cairo_move_to(cr, 0.0, -10.0);
       // cairo_line_to(cr, 0.0, -40.0);
       // cairo_rotate(cr, M_PI/4);
@@ -77,8 +89,6 @@ static void do_drawing(cairo_t *cr, GtkWidget *widget)
       // cairo_stroke(cr);
   }
 }
-
-
 
 
 static gboolean time_handler(GtkWidget *widget)
